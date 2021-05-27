554427836fcc2910f947874f7192ee40d9bdd0c0
MahamadMudassar <yadwad.mk@outlook.com>
2021-05-27 14:47:38 +0530

    product component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 //feature 1
+import React from 'react';
+import data from './data.json';
 import './App.css';
+import Products from './components/Products';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      products:data.products,
+      size:"",
+      sort:""
+
+    }
+  }
+  click(){
+    console.log("gii");
+    console.log(this.state.products);;
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>My page</h2>
+    <div className="grid-container">
+      <header className="header">
+        <a href="/">React Shopping Cart</a>
       </header>
+      <main>
+        < div className="content">
+            <div className="main">
+              <Products products={this.state.products}></Products>
+            </div>
+            <div className="sidebar"> Cart items</div>
+        </div>
+      </main>
+      <footer>All rights reserved</footer>
     </div>
   );
+}
 }
 
 export default App;
